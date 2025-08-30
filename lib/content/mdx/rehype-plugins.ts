de81d2ca3d80e6672aa1aa8eb43315c9fe8a6d48
Stefan Probst <stefanprobst@posteo.de>
2025-08-30 11:15:45 +0200

    Revert "remove mermaid"
    This reverts commit a94a504c74ac787239389356e5e0fb21478060e1.

--- lib/content/mdx/rehype-plugins.ts
+++ lib/content/mdx/rehype-plugins.ts
@@ -8,7 +8,7 @@ import {
 	withUnwrappedMdxFlowContent,
 } from "@acdh-oeaw/mdx-lib";
 import withSyntaxHighlighter, { type RehypeShikiOptions } from "@shikijs/rehype";
-// import withMermaidDiagrams from "rehype-mermaid";
+import withMermaidDiagrams from "rehype-mermaid";
 import withHeadingIds from "rehype-slug";
 import type { Pluggable } from "unified";
 
@@ -45,15 +45,15 @@ export function createRemoteImageUrlsPlugin(baseUrl: string, components?: Array<
 	return [withRemoteImageUrls, { baseUrl, components }] satisfies Pluggable;
 }
 
-// export function createMermaidDiagramsPlugin() {
-// 	return [
-// 		withMermaidDiagrams,
-// 		{
-// 			mermaidConfig: { fontFamily: '"Roboto", system-ui, sans-serif' },
-// 			strategy: "inline-svg",
-// 		},
-// 	] satisfies Pluggable;
-// }
+export function createMermaidDiagramsPlugin() {
+	return [
+		withMermaidDiagrams,
+		{
+			mermaidConfig: { fontFamily: '"Roboto", system-ui, sans-serif' },
+			strategy: "inline-svg",
+		},
+	] satisfies Pluggable;
+}
 
 export function createSyntaxHighlighterPlugin() {
 	return [withSyntaxHighlighter, syntaxHighlighterConfig] satisfies Pluggable;
