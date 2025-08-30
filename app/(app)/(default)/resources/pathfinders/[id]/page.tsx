586aea59be424a070ec6b309fecd672273d652ce
Stefan Probst <stefanprobst@posteo.de>
2025-08-30 10:33:13 +0200

    Revert "Reapply "remove dynamicparams""
    This reverts commit 29a43989fd344214bf3ead5964502157ca23990d.

--- app/(app)/(default)/resources/pathfinders/[id]/page.tsx
+++ app/(app)/(default)/resources/pathfinders/[id]/page.tsx
@@ -23,6 +23,8 @@ import { getMetadata } from "@/lib/i18n/metadata";
 import { createFullUrl } from "@/lib/navigation/create-full-url";
 import { pickRandom } from "@/lib/utils/pick-random";
 
+export const dynamicParams = false;
+
 interface PathfinderResourcePageProps extends PageProps<"/resources/pathfinders/[id]"> {}
 
 export function generateStaticParams(): Array<
